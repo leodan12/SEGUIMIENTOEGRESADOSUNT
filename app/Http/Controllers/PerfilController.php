2bf1b5981d401e72e847bea2349213c2c47a141d
leodan12 <leomacip4@gmail.com>
2021-11-17 15:44:09 -0500

    subir todo

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Perfil;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;   //siempre poner esto
+use Illuminate\Support\Facades\Auth;   //siempre poner esto ....
 use Illuminate\Support\Facades\Hash;
 
 class PerfilController extends Controller
