8b606b599a700c243be0054392ca0656c80dc31d
leodan12 <leomacip4@gmail.com>
2021-11-28 02:05:04 -0500

    subido bien

--- routes/web.php
+++ routes/web.php
@@ -44,10 +44,10 @@ Route::get('/perfiles', 'PerfilController@index');
 Route::resource('perfil','PerfilController');
 
 
-/*
+
 Route::get('/', function () {
     return view('welcome');
-});*/
+});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
